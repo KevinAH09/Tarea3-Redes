f7b3b200cce5afa3d6df17c670a37a00a8ae0e4a
KevinAH09 <55261485+KevinAH09@users.noreply.github.com>
2020-06-01 20:01:18 -0600

    cm

--- Tarea3/Tarea3/Tarea3.py
+++ Tarea3/Tarea3/Tarea3.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 import os
-
+from tkinter import messagebox
 
 
 
@@ -11,10 +11,7 @@ entry_var = tk.StringVar()
 
 
 def hola():
-    print(entry_var.get())
-
-
-
+    messagebox.showinfo(title="Envio", message= "El mensaje"+entry_var+ "sera enviado" )
 
 ventana.title("Cliente")
 ventana.minsize(800,600)
@@ -23,20 +20,20 @@ ventana.maxsize(800,600)
 ruta = os.getcwd()
 ruta1 = ruta.replace("\\", "\\\\")
 
-filename = tk.PhotoImage(file = ruta1+"\\\\fondo4.png")
-filename.zoom(1,1)
-background_label = tk.Label(ventana,image=filename,bg="dodger blue")
+
+filename = tk.PhotoImage(file = ruta1+"\\\\fondo3.png")
+background_label = tk.Label(ventana,bg="blue",image=filename)
 background_label.place(x=0, y=0, relwidth=1, relheight=1)
 
-texBox1 = tk.Label(ventana,text="Ingrese el Mensaje",bg = "blue", fg = "white")
-texBox1.place(x=50, y=10)
+
+texBox1 = tk.Label(ventana,text="Ingrese el mensaje",bg = "RoyalBlue3", fg = "black")
+texBox1.place(x=350, y=150,width=100,height=30)
+
 entry = tk.Entry(ventana,textvariable = entry_var)
-entry.place(x=50, y=50)
+entry.place(x=150, y=300,width=500,height=20)
 
-
-Button = tk.Button(ventana, text="Clícame", command=hola)
-Button.place(x=50, y=100)
-
+Button = tk.Button(ventana, text="ENVIAR", command=hola)
+Button.place(x=350, y=350,width=100,height=20)
 
 
 ventana.mainloop()
