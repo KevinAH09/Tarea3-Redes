0744ff7e3039179d6b9d9d567d8baa5a1e024504
KevinAH09 <55261485+KevinAH09@users.noreply.github.com>
2020-06-02 12:22:20 -0600

    cm

--- Tarea3/Tarea3/Tarea3.py
+++ Tarea3/Tarea3/Tarea3.py
@@ -1,17 +1,265 @@
 import tkinter as tk
 import os
 from tkinter import messagebox
+Acodi="Ø" 
+Bcodi="Ω"
+Ccodi = "Σ"
+Dcodi="ø"
+Ecodi="º"
+Fcodi="◊"
+Gcodi="¬"
+Hcodi="«"
+Icodi="»"
 
+Jcodi="▓"
+Kcodi="┤"
+Lcodi ="╣"
+Mcodi="╝"
+Ncodi="¥"
+Ñcodi="┴"
+Ocodi="ð"
+Pcodi="Þ"
+Qcodi="≡"
+Rcodi="±"
+Scodi="¶"
+Tcodi="§"
+Ucodi="$"
+Vcodi="%"
+Wcodi="^"
+Xcodi="&"
+Ycodi="Æ"
+Zcodi="₫"
+PUNTOcodi = "■"
+ESPACIOcodi="_"
+COMAcodi = "*"
+
+UNOcodi = "☏"
+DOScodi = "☆"
+TREScodi = "★"
+CUATROcodi = "▽"
+CINCOcodi = "△"
+SEIScodi = "♡"
+SIETEcodi = "∵"
+OCHOcodi = "∴"
+NUEVEcodi = "∷"
+CEROcodi = "♤"
 
 
 
 ventana = tk.Tk()
 entry_var = tk.StringVar()
+cadena = ""
+cadenaDecodi = ""
 
+lista = []
+class CapaTransporte:
+
+    def init(self):
+        self.n = ""
+        self.id = 0
+        self.lis = []
+
+
+    def multiplexar(self,cadena):
+        cont = 1
+        for u in cadena:
+            self.lis = []
+            self.n=u
+            self.id=cont
+            self.lis.append(u)
+            self.lis.append(cont)
+            lista.append(self.lis)
+            cont=cont+1
+        print(lista)
+    """def hola():Esta aqui por que despues vemos como lo llamamos
+        #messagebox.showinfo(title="Envio", message= "El mensaje "+entry_var.get()+ " sera enviado" )
+        transporte = CapaTransporte()
+        transporte.multiplexar(entry_var.get())"""
+
+
+
+class CapaPresentacion:
+    def __init__(self):
+        cadena = ""
+        cadenaDecodi = ""
+    def codificar(self,mensaje):
+        global cadena
+        for i in mensaje:
+            if i == "A":
+                cadena = cadena + Acodi
+            elif i == "B":
+                cadena = cadena + Bcodi
+            elif i == "C":
+                cadena = cadena + Ccodi
+            elif i == "D":
+                cadena = cadena + Dcodi
+            elif i == "E":
+                cadena = cadena + Ecodi
+            elif i == "F":
+                cadena = cadena + Fcodi
+            elif i == "G":
+                cadena = cadena + Gcodi
+            elif i == "H":
+                cadena = cadena + Hcodi
+            elif i == "I":
+                cadena = cadena + Icodi
+            elif i == "J":
+                cadena = cadena + Jcodi
+            elif i == "K":
+                cadena = cadena + Kcodi
+            elif i == "L":
+                cadena = cadena + Lcodi
+            elif i == "M":
+                cadena = cadena + Mcodi
+            elif i == "N":
+                cadena = cadena + Ncodi
+            elif i == "O":
+                cadena = cadena + Ocodi
+            elif i == "P":
+                cadena = cadena + Pcodi
+            elif i == "Q":
+                cadena = cadena + Qcodi
+            elif i == "R":
+                cadena = cadena + Rcodi
+            elif i == "S":
+                cadena = cadena + Scodi
+            elif i == "T":
+                cadena = cadena + Tcodi
+            elif i == "U":
+                cadena = cadena + Ucodi
+            elif i == "V":
+                cadena = cadena + Vcodi
+            elif i == "W":
+                cadena = cadena + Wcodi
+            elif i == "X":
+                cadena = cadena + Xcodi
+            elif i == "Y":
+                cadena = cadena + Ycodi
+            elif i == "Z":
+                cadena = cadena + Zcodi
+            elif i == "1":
+                cadena = cadena + UNOcodi
+            elif i == "2":
+                cadena = cadena + DOScodi
+            elif i == "3":
+                cadena = cadena + TREScodi
+            elif i == "4":
+                cadena = cadena + CUATROcodi
+            elif i == "5":
+                cadena = cadena + CINCOcodi
+            elif i == "6":
+                cadena = cadena + SEIScodi
+            elif i == "7":
+                cadena = cadena + SIETEcodi
+            elif i == "8":
+                cadena = cadena + OCHOcodi
+            elif i == "9":
+                cadena = cadena + NUEVEcodi
+            elif i == "0":
+                cadena = cadena + CEROcodi
+            elif i == ".":
+                cadena = cadena + PUNTOcodi
+            elif i == ",":
+                cadena = cadena + COMAcodi
+            elif i == " ":
+                cadena = cadena + ESPACIOcodi
+            else:
+                cadena = cadena + i;
+        print(cadena)
+        return cadena
+
+    def decodificor(self,mensaje):
+        global cadenaDecodi
+        for i in mensaje:
+            if i == Acodi:
+                cadenaDecodi = cadenaDecodi + "A"
+            elif i == Bcodi:
+                cadenaDecodi = cadenaDecodi + "B"
+            elif i == Ccodi:
+                cadenaDecodi = cadenaDecodi + "C"
+            elif i == Dcodi:
+                cadenaDecodi = cadenaDecodi + "D"
+            elif i == Ecodi:
+                cadenaDecodi = cadenaDecodi + "E"
+            elif i == Fcodi:
+                cadenaDecodi = cadenaDecodi + "F"
+            elif i == Gcodi:
+                cadenaDecodi = cadenaDecodi + "G"
+            elif i == Hcodi:
+                cadenaDecodi = cadenaDecodi + "H"
+            elif i == Icodi:
+                cadenaDecodi = cadenaDecodi + "I"
+            elif i == Jcodi:
+                cadenaDecodi = cadenaDecodi + "J"
+            elif i == Kcodi:
+                cadenaDecodi = cadenaDecodi + "K"
+            elif i == Lcodi:
+                cadenaDecodi = cadenaDecodi + "L"
+            elif i == Mcodi:
+                cadenaDecodi = cadenaDecodi + "M"
+            elif i == Ncodi:
+                cadenaDecodi = cadenaDecodi + "N"
+            elif i == Ocodi:
+                cadenaDecodi = cadenaDecodi + "O"
+            elif i == Pcodi:
+                cadenaDecodi = cadenaDecodi + "P"
+            elif i == Qcodi:
+                cadenaDecodi = cadenaDecodi + "Q"
+            elif i == Rcodi:
+                cadenaDecodi = cadenaDecodi + "R"
+            elif i == Scodi:
+                cadenaDecodi = cadenaDecodi + "S"
+            elif i == Tcodi:
+                cadenaDecodi = cadenaDecodi + "T"
+            elif i == Ucodi:
+                cadenaDecodi = cadenaDecodi + "U"
+            elif i == Vcodi:
+                cadenaDecodi = cadenaDecodi + "V"
+            elif i == Wcodi:
+                cadenaDecodi = cadenaDecodi + "W"
+            elif i == Xcodi:
+                cadenaDecodi = cadenaDecodi + "X"
+            elif i == Ycodi:
+                cadenaDecodi = cadenaDecodi + "Y"
+            elif i == Zcodi:
+                cadenaDecodi = cadenaDecodi + "Z"
+            elif i == UNOcodi:
+                cadenaDecodi = cadenaDecodi + "1"
+            elif i == DOScodi:
+                cadenaDecodi = cadenaDecodi + "2"
+            elif i == TREScodi:
+                cadenaDecodi = cadenaDecodi + "3"
+            elif i == CUATROcodi:
+                cadenaDecodi = cadenaDecodi + "4"
+            elif i == CINCOcodi:
+                cadenaDecodi = cadenaDecodi + "5"
+            elif i == SEIScodi:
+                cadenaDecodi = cadenaDecodi + "6"
+            elif i == SIETEcodi:
+                cadenaDecodi = cadenaDecodi + "7"
+            elif i == OCHOcodi:
+                cadenaDecodi = cadenaDecodi + "8"
+            elif i == NUEVEcodi:
+                cadenaDecodi = cadenaDecodi + "9"
+            elif i == CEROcodi:
+                cadenaDecodi = cadenaDecodi + "0"
+            elif i == PUNTOcodi:
+                cadenaDecodi = cadenaDecodi + "."
+            elif i == COMAcodi:
+                cadenaDecodi = cadenaDecodi + ","
+            elif i == ESPACIOcodi:
+                cadenaDecodi = cadenaDecodi + " "
+            else:
+                cadenaDecodi = cadenaDecodi + i;
+        print(cadenaDecodi)
 
 
 def hola():
-    messagebox.showinfo(title="Envio", message= "El mensaje"+entry_var+ "sera enviado" )
+    codi = CapaPresentacion()
+    men = ""
+    print(entry_var.get())
+    men = entry_var.get()
+    codi.decodificor( codi.codificar(men))
 
 ventana.title("Cliente")
 ventana.minsize(800,600)
@@ -36,4 +284,8 @@ Button = tk.Button(ventana, text="ENVIAR", command=hola)
 Button.place(x=350, y=350,width=100,height=20)
 
 
+
+
+
 ventana.mainloop()
+
